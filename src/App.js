b0f3b752744ca145c538d6e6539799899d9e1ada
ShivaShankar <ss3177806@gmail.com>
2024-07-13 17:47:01 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -25,17 +25,19 @@ const Body = styled.div`
 
 const Wrapper = styled.div`
   background: linear-gradient(
-      38.73deg,
-      rgba(204, 0, 187, 0.15) 0%,
-      rgba(201, 32, 184, 0) 50%
+      45deg,
+      rgba(0, 173, 181, 0.1) 0%,
+      rgba(0, 173, 181, 0.05) 50%,
+      rgba(0, 173, 181, 0.02) 100%
     ),
     linear-gradient(
-      141.27deg,
-      rgba(0, 70, 209, 0) 50%,
-      rgba(0, 70, 209, 0.15) 100%
+      135deg,
+      rgba(0, 150, 158, 0.02) 0%,
+      rgba(0, 150, 158, 0.05) 50%,
+      rgba(0, 150, 158, 0.1) 100%
     );
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 100% 100%, 0 100%);
 `;
 
 function App() {
